dead5290e46b1c048342166a96d13000ea5e05b1
Vu Pham <vupham326@gmail.com>
2019-03-28 08:54:32 -0700

    commit codes for banking application

---
+++ SGBank.BLL/DepositRules/FreeAccountDepositRule.cs
@@ -0,0 +1,50 @@
+﻿using SGBank.Models;
+using SGBank.Models.Responses;
+using System;
+using SGBank.Models.Interfaces;
+
+namespace SGBank.BLL.DepositRules
+{
+    public class FreeAccountDepositRule : IDeposit
+    {
+        public AccountDepositResponse Deposit(AccountManager account, decimal amount)
+        {
+            AccountDepositResponse response = new AccountDepositResponse();
+
+            if(account.Type != AccountType.Free)
+            {
+                response.Success = false;
+                response.Message = "Error: a non free account hit the Free Deposit Rule. Contact IT";
+                return response;
+            }
+
+            if(amount > 100)
+            {
+                response.Success = false;
+                response.Message = "Free accounts cannot deposit more than 100 at a time";
+                return response;
+            }
+
+            if(amount <= 0)
+            {
+                response.Success = false;
+                response.Message = "Deposit amounts must be greater than zero.";
+                return response;
+            }
+
+            response.OldBalance = account.Balance;
+            account.Balance += amount;
+            response.Account = account.Type;
+            response.Amount = amount;
+            response.Success = true;
+
+            return response;
+
+        }
+
+        AccountDepositResponse IDeposit.Deposit(Account account, decimal amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SGBank.UI/Menu.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SGBank.UI.Workflows;
+
+namespace SGBank.UI
+{
+    public static class Menu
+    {
+        public static object AccountLookupWorkflow { get; private set; }
+
+        public static void Start()
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("SG Bank Application");
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("1. Lookup an Account");
+                Console.WriteLine("2. Deposit");
+                Console.WriteLine("3.Withdraw");
+
+                Console.WriteLine("\nQ to quit");
+                Console.Write("\nEnter Selection: ");
+
+                string userinput = Console.ReadLine();
+                switch (userinput)
+                {
+                    case "1":
+                        AccountLookUpWorkflow lookupWorkflow = new AccountLookUpWorkflow();
+                        lookupWorkflow.Execute();
+                        break;
+                    case "2":
+                        DepositWorkflow depositWorkflow = new DepositWorkflow();
+                        depositWorkflow.Execute();
+                        break;
+                    case "3":
+                        break;
+                    case "Q":
+                        return;
+                
+                }
+
+            }
+        }
+    }
+}
